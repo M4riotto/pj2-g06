9dfdbf3cbe76369f46218c11b3a967f1050a50cc
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2022-12-02 15:31:51 -0300

    front esqueceu senha .php

--- pj2-g06/backend/esqueceu_a_senha.php
+++ pj2-g06/backend/esqueceu_a_senha.php
@@ -20,11 +20,11 @@
             
                 $row_usuario = $query_alter->fetch(PDO::FETCH_ASSOC);//lendo o valor com o fetch
                 var_dump($row_usuario);
-                if($row_usuario == 0){
-                    $erro[] = "<span style='color: #ff0000'>Erro: Usuário ou senha inválida!</span>";
+                if($row_usuario == 0 ){
+                    echo $erro[] = "<span style='color: #ff0000'>Erro: Usuário ou senha inválida!</span>";
                     }
                 if ($row_usuario != 0) {
-                    $novasenha = substr(time(), 0, 6);                   
+                    $novasenha = substr(md5(time()), 0, 6);                   
                     $senhacripto = password_hash($novasenha, PASSWORD_DEFAULT);
                     echo($novasenha);
 //mail($email, "Sua nova senha", "Sua nova senha é: ".$senhacripto)
@@ -39,7 +39,9 @@
                         echo $_SESSION['sucess'] = 'Senha alterada com sucesso';
                         }
                 }
-    }}
+    }else {
+                    echo $_SESSION['error'] = "<span style='color: #ff0000'>Erro: Usuário ou senha inválida!</span>";
+                }}
 
 ?>
 
@@ -50,13 +52,64 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recuperação de senha</title>
+    <style>
+        body, html{
+            background-color: #3ecbff;
+            height: 100%;
+        }
+        body{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        form > input{
+            border-radius: 7px;
+            border: 2px solid #ffbb9a;
+            padding: 10px;
+        }
+        form{
+            display:flex;
+            flex-direction: column;
+
+        }
+
+        form > button{
+            padding: 5px;
+            border: 2px solid #ffbb9a;
+        }
+
+        form > button:hover{
+            background-color: #007affcc;
+        }
+        a > button{
+            padding: 5px;
+            border: 2px solid #ffbb9a;
+            width: 100%;
+        }
+        a > button:hover{
+            background-color: #007affcc;
+        }
+
+        .caixa{
+            background: white;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: aliceblue;
+            box-shadow: 8px 8px 8px 7px black;
+        }
+    </style>
 </head>
 <body>
+    <div class="caixa">
     <form action="" method="post">
+        <h2>Recuperação de senha</h2>
         <label for="email"></label>
-        <input type="email" name="email" placeholder="Digite seu e-mail">
-        <button type="submit" name="ok" value="ok">ok</button>
-    </form>
+        <input type="email" name="email" placeholder="Digite seu e-mail"><br>
+        <button type="submit" name="ok" value="ok">Enviar</button>
+        
+    </form><br>
+    <a href="index.php"><button>Voltar</button></a></div>
     
 </body>
 </html>
