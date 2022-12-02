70abdb77f16b1b929b7c59d2a47846f7c74ac0c4
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2022-12-02 13:56:43 -0300

    recuperação de senha

--- pj2-g06/backend/esqueceu_a_senha.php
+++ pj2-g06/backend/esqueceu_a_senha.php
@@ -1,35 +1,43 @@
 <?php
+    session_start();
     require "database/database.php";
+
+    $erro = array();
     
-    $email = filter_input_array(INPUT_POST, FILTER_DEFAULT);;
+    $email = filter_input_array(INPUT_POST, FILTER_DEFAULT);
     if (!empty($email['ok'])) { //empty = vazio || só acessa esse if qnd o usuario clicar no botao
+        $email = $_POST['email'];
+        
         $query_senha = "SELECT id, senha_usuario 
                           FROM usuarios 
-                          WHERE email = :email 
-                          LIMIT 1"; //salvando esse texto na variavel
+                          WHERE email = '$email'"; //salvando esse texto na variavel
 
         $query_alter = $connect->prepare($query_senha);//fazendo o select no BDD
-        $query_alter->bindParam(':email', $email['email'], PDO::PARAM_STR);//bind param = substituir o valor
+       
         $query_alter->execute();//executando a query
 
             if(($query_alter) AND ($query_alter->rowCount() != 0)){//se a qtnd de linha que encontrou no BDD for ! 0,     então acessar o IF 
             
                 $row_usuario = $query_alter->fetch(PDO::FETCH_ASSOC);//lendo o valor com o fetch
-                //var_dump($row_usuario);
-                if($row_usuario == 0)
-                $erro[] = "o email informado nao existe";
-
-                if (!isset($erro) && $row_usuario != 0) {
-                    $novasenha = substr(md5(time()), 0, 6);
-                    $senhacripto = md5(md5($novasenha));
+                var_dump($row_usuario);
+                if($row_usuario == 0){
+                    $erro[] = "<span style='color: #ff0000'>Erro: Usuário ou senha inválida!</span>";
+                    }
+                if ($row_usuario != 0) {
+                    $novasenha = substr(time(), 0, 6);                   
+                    $senhacripto = password_hash($novasenha, PASSWORD_DEFAULT);
+                    echo($novasenha);
 //mail($email, "Sua nova senha", "Sua nova senha é: ".$senhacripto)
                     if (1==1) {
-                        $stmt = "UPDATE usuarios set senha_usuario = '$senhacripto' WHERE email = '$email[email]'";
+                        $update = "UPDATE usuarios set senha_usuario = '$senhacripto' WHERE email = '$email'";
                         
-                        $update = $connect->prepare($stmt);//fazendo o select no BDD
-                        $update->bindParam(':senha_usuario', $email['senha'], PDO::PARAM_STR);//bind param = substituir o valor
-                        $query_alter->execute();
-                                }
+                        $stmt = $connect->prepare($update);//fazendo o select no BDD
+
+                        $stmt->execute();
+
+                        echo'<br>';
+                        echo $_SESSION['sucess'] = 'Senha alterada com sucesso';
+                        }
                 }
     }}
 
@@ -47,8 +55,6 @@
     <form action="" method="post">
         <label for="email"></label>
         <input type="email" name="email" placeholder="Digite seu e-mail">
-        <label for="senha"></label>
-        <input type="password" name="senha" placeholder="Digite sua nova senha">
         <button type="submit" name="ok" value="ok">ok</button>
     </form>
     
